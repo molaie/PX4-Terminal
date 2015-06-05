1f89638bd8804ec60bca6142d2b9af07c83df149
ahmadmolaie <molaie@gmail.com>
2015-06-06 01:00:46 +0430

    just commit

--- PX4-Terminal/sha1-FP/Form1.Designer.cs
+++ PX4-Terminal/sha1-FP/Form1.Designer.cs
@@ -23,12 +23,73 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.txtLog = new System.Windows.Forms.RichTextBox();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.btnClearLog = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtLog
+			// 
+			this.txtLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.txtLog.Location = new System.Drawing.Point(0, 41);
+			this.txtLog.Name = "txtLog";
+			this.txtLog.Size = new System.Drawing.Size(784, 337);
+			this.txtLog.TabIndex = 0;
+			this.txtLog.Text = "";
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(249, 12);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(75, 23);
+			this.btnConnect.TabIndex = 1;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(22, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(201, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// btnClearLog
+			// 
+			this.btnClearLog.Location = new System.Drawing.Point(331, 12);
+			this.btnClearLog.Name = "btnClearLog";
+			this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+			this.btnClearLog.TabIndex = 3;
+			this.btnClearLog.Text = "Clear Log";
+			this.btnClearLog.UseVisualStyleBackColor = true;
+			this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(784, 378);
+			this.Controls.Add(this.btnClearLog);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.txtLog);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "PX4Terminal";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.RichTextBox txtLog;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button btnClearLog;
 	}
 }
 
--- PX4-Terminal/sha1-FP/Form1.cs
+++ PX4-Terminal/sha1-FP/Form1.cs
@@ -13,5 +13,40 @@ namespace sha1_FP {
 		public Form1() {
 			InitializeComponent();
 		}
+
+		private void btnConnect_Click(object sender, EventArgs e) {
+
+		}
+
+		private void btnClearLog_Click(object sender, EventArgs e) {
+			txtLog.Clear();
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		public new static string[] GetPortNames() {
+				string[] ports = null;
+
+				try {
+					ports = System.IO.Ports.SerialPort.GetPortNames()
+					.Select(p => p.TrimEnd())
+					
+					.ToArray();
+				} catch { }
+
+				return ports.ToArray();
+			}
+		}
+
+
 	}
 }
