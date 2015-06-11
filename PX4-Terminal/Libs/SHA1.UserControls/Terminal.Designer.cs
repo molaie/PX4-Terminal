88a318914ba01e12eef2ab3e9a3ca0be23d7f798
ahmadmolaie <molaie@gmail.com>
2015-06-11 16:52:01 +0430

    Terminal keyup handler

--- PX4-Terminal/Libs/SHA1.UserControls/Terminal.Designer.cs
+++ PX4-Terminal/Libs/SHA1.UserControls/Terminal.Designer.cs
@@ -23,93 +23,37 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.greenOnBlackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.whiteOnBlackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.blackOnWhiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.customToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-			this.contextMenuStrip1.SuspendLayout();
+			this.txtTerminal = new System.Windows.Forms.RichTextBox();
 			this.SuspendLayout();
 			// 
-			// contextMenuStrip1
+			// txtTerminal
 			// 
-			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.colorToolStripMenuItem});
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
-			// 
-			// colorToolStripMenuItem
-			// 
-			this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.greenOnBlackToolStripMenuItem,
-            this.whiteOnBlackToolStripMenuItem,
-            this.blackOnWhiteToolStripMenuItem,
-            this.customToolStripMenuItem});
-			this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
-			this.colorToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-			this.colorToolStripMenuItem.Text = "Color";
-			// 
-			// greenOnBlackToolStripMenuItem
-			// 
-			this.greenOnBlackToolStripMenuItem.Checked = true;
-			this.greenOnBlackToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.greenOnBlackToolStripMenuItem.Name = "greenOnBlackToolStripMenuItem";
-			this.greenOnBlackToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
-			this.greenOnBlackToolStripMenuItem.Text = "&Green on Black";
-			// 
-			// whiteOnBlackToolStripMenuItem
-			// 
-			this.whiteOnBlackToolStripMenuItem.Name = "whiteOnBlackToolStripMenuItem";
-			this.whiteOnBlackToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
-			this.whiteOnBlackToolStripMenuItem.Text = "&White on Black";
-			// 
-			// blackOnWhiteToolStripMenuItem
-			// 
-			this.blackOnWhiteToolStripMenuItem.Name = "blackOnWhiteToolStripMenuItem";
-			this.blackOnWhiteToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
-			this.blackOnWhiteToolStripMenuItem.Text = "&Black on White";
-			// 
-			// customToolStripMenuItem
-			// 
-			this.customToolStripMenuItem.Name = "customToolStripMenuItem";
-			this.customToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
-			this.customToolStripMenuItem.Text = "&Custom";
-			// 
-			// richTextBox1
-			// 
-			this.richTextBox1.BackColor = System.Drawing.Color.Black;
-			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.richTextBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
-			this.richTextBox1.Location = new System.Drawing.Point(0, 0);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(597, 376);
-			this.richTextBox1.TabIndex = 1;
-			this.richTextBox1.Text = "adsasda";
+			this.txtTerminal.BackColor = System.Drawing.Color.Black;
+			this.txtTerminal.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtTerminal.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtTerminal.ForeColor = System.Drawing.Color.Lime;
+			this.txtTerminal.Location = new System.Drawing.Point(0, 0);
+			this.txtTerminal.Name = "txtTerminal";
+			this.txtTerminal.Size = new System.Drawing.Size(754, 432);
+			this.txtTerminal.TabIndex = 0;
+			this.txtTerminal.Text = "ads";
+			this.txtTerminal.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtTerminal_KeyDown);
+			this.txtTerminal.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTerminal_KeyPress);
+			this.txtTerminal.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtTerminal_KeyUp);
 			// 
 			// Terminal
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.txtTerminal);
 			this.Name = "Terminal";
-			this.Size = new System.Drawing.Size(597, 376);
-			this.contextMenuStrip1.ResumeLayout(false);
+			this.Size = new System.Drawing.Size(754, 432);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-		private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem greenOnBlackToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem whiteOnBlackToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem blackOnWhiteToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem customToolStripMenuItem;
-		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox txtTerminal;
 	}
 }
--- PX4-Terminal/Libs/SHA1.UserControls/Terminal.cs
+++ PX4-Terminal/Libs/SHA1.UserControls/Terminal.cs
@@ -10,8 +10,50 @@ using System.Windows.Forms;
 
 namespace SHA1.UserControls {
 	public partial class Terminal : UserControl {
+
+
+		List<string> cmdHistory = new List<string>();
+		int history = 0;
+		//save last position of caret, so user can go up & down in history
+		int inputStartPos = 0;
+
 		public Terminal() {
 			InitializeComponent();
 		}
+
+		private void txtTerminal_KeyPress(object sender, KeyPressEventArgs e) {
+
+		}
+
+		private void txtTerminal_KeyDown(object sender, KeyEventArgs e) {
+			txtTerminal.SelectionStart = txtTerminal.Text.Length;
+			switch (e.KeyData) {
+				case Keys.Up:
+					if (history > 0) {
+						txtTerminal.Select(inputStartPos, txtTerminal.Text.Length - inputStartPos);
+						txtTerminal.SelectedText = "";
+						txtTerminal.AppendText(cmdHistory[--history]);
+					}
+					e.Handled = true;
+					break;
+				case Keys.Down:
+					if (history < cmdHistory.Count - 1) {
+						txtTerminal.Select(inputStartPos, txtTerminal.Text.Length - inputStartPos);
+						txtTerminal.SelectedText = "";
+						txtTerminal.AppendText(cmdHistory[++history]);
+					}
+					e.Handled = true;
+					break;
+				case Keys.Left:
+				case Keys.Back:
+					if (txtTerminal.SelectionStart <= inputStartPos)
+						e.Handled = true;
+					break;
+
+				//case Keys.Right:
+				//    break;
+			}
+		}
+
 	}
 }
