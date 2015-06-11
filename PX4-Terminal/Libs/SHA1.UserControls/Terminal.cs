337a020a172093adad94afb16a60d73ed98e6c22
ahmadmolaie <molaie@gmail.com>
2015-06-11 16:29:09 +0430

    move to new folder

---
+++ PX4-Terminal/Libs/SHA1.UserControls/Terminal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SHA1.UserControls {
+	public partial class Terminal : UserControl {
+		public Terminal() {
+			InitializeComponent();
+		}
+	}
+}
